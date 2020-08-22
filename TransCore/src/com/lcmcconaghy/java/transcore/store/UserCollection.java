1f6aa8c26989f0679a5c94ae6154f4b2cef12299
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-08-22 13:24:11 -0400

    Broadened scope of 'get' method

--- TransCore/src/com/lcmcconaghy/java/transcore/store/UserCollection.java
+++ TransCore/src/com/lcmcconaghy/java/transcore/store/UserCollection.java
@@ -2,8 +2,6 @@ package com.lcmcconaghy.java.transcore.store;
 
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class UserCollection<T extends UserItem> extends StoreCollection<T>
@@ -58,57 +56,37 @@ public class UserCollection<T extends UserItem> extends StoreCollection<T>
 	// { GET } //
 	
 	/**
-	 * Get UserItem from String username
-	 * @return UserItem
+	 * Get the Generic Type from a broad Object argument
+	 * @param arg0 Player, UUID or String id
+	 * @return matching Generic Type extending UserItem
 	 */
-	public T get(String arg0)
+	public T get(Object arg0)
 	{
-		T ret = null;
+		String id = null;
+		T item = null;
+		
+		if (arg0 instanceof String)
+		{
+			id = (String) arg0;
+		}
+		else if (arg0 instanceof UUID)
+		{
+			id = ((UUID)arg0).toString();
+		}
+		else if (arg0 instanceof Player)
+		{
+			id = ((Player)arg0).getUniqueId().toString();
+		}
 		
 		for (T user : this)
 		{
-			UUID id = UUID.fromString(user.getID());
-			OfflinePlayer player = Bukkit.getOfflinePlayer(id);
+			if (!user.id.equalsIgnoreCase(id)) continue;
 			
-			if (!player.getName().equalsIgnoreCase(arg0)) continue;
-			
-			ret = user;
+			item = user;
+			break;
 		}
 		
-		return ret;
-	}
-	
-	/**
-	 * Get UserItem from UUID
-	 * @param arg0 UUID of UserItem
-	 * @return UserItem
-	 */
-	public T get(UUID arg0)
-	{
-		T ret = null;
-		
-		for (T items : this)
-		{
-			UUID compare = UUID.fromString(items.getID());
-			
-			if (!arg0.equals(compare)) continue;
-			
-			ret = items;
-		}
-		
-		return ret;
-	}
-	
-	/**
-	 * Get UserItem from Player
-	 * @param arg0 Player to get UserItem from
-	 * @return UserItem
-	 */
-	public T get(Player arg0)
-	{
-		UUID id = arg0.getUniqueId();
-		
-		return get(id);
+		return item;
 	}
 	
 }
