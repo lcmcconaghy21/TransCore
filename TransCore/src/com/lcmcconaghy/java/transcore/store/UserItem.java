7351285adf858195d5f91a9b967db70383f0acb2
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-08-23 01:00:57 -0400

    Add nearby entity or Player

--- TransCore/src/com/lcmcconaghy/java/transcore/store/UserItem.java
+++ TransCore/src/com/lcmcconaghy/java/transcore/store/UserItem.java
@@ -1,5 +1,9 @@
 package com.lcmcconaghy.java.transcore.store;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import com.lcmcconaghy.java.transcore.Message;
@@ -23,6 +27,30 @@ public class UserItem extends StoreItem<UserItem>
 	public boolean isNpc()
 	{
 		return this.getPlayer().hasMetadata("NPC");
+	}
+	
+	// { LOCATION } //
+	
+	/**
+	 * Get HashSet of nearby Players within radius
+	 * @param arg0 Double radius
+	 * @return HashSet of nearby Players
+	 */
+	public Set<Player> getNearbyPlayers(double arg0)
+	{
+		if (!this.player.isOnline()) return null;
+		
+		Set<Player> nearbyPlayers = new HashSet<Player>();
+		
+		for (Entity part : player.getNearbyEntities(arg0, arg0, arg0))
+		{
+			if (!(part instanceof Player)) continue;
+			
+			Player player = (Player) part;
+			nearbyPlayers.add(player);
+		}
+		
+		return nearbyPlayers;
 	}
 	
 	// { CHAT } //
