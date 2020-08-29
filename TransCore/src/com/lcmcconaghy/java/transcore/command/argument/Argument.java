e3c8e636068521f04a3a6588e2e2f93efbc0b7da
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-08-28 21:12:32 -0400

    Arguments can now throw TransCommandException when returning null

--- TransCore/src/com/lcmcconaghy/java/transcore/command/argument/Argument.java
+++ TransCore/src/com/lcmcconaghy/java/transcore/command/argument/Argument.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.bukkit.command.CommandSender;
 
 import com.lcmcconaghy.java.transcore.command.TransCommand;
+import com.lcmcconaghy.java.transcore.exception.TransCommandException;
 
 public interface Argument<T>
 {
 	
-	public T read(String arg0, CommandSender arg1);
+	public T read(String arg0, CommandSender arg1) throws TransCommandException;
 	
 	public List<String> getTabCompleteList();
 	
--- TransCore/src/com/lcmcconaghy/java/transcore/command/argument/bukkit/ArgumentUser.java
+++ TransCore/src/com/lcmcconaghy/java/transcore/command/argument/bukkit/ArgumentUser.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.bukkit.command.CommandSender;
 
 import com.lcmcconaghy.java.transcore.command.argument.ArgumentAbstract;
+import com.lcmcconaghy.java.transcore.exception.TransCommandException;
 import com.lcmcconaghy.java.transcore.store.UserCollection;
 import com.lcmcconaghy.java.transcore.store.UserItem;
 
@@ -25,8 +26,13 @@ public class ArgumentUser<T extends UserItem> extends ArgumentAbstract<T>
 	// { ARGUMENT } //
 	
 	@Override
-	public T read(String arg0, CommandSender arg1)
+	public T read(String arg0, CommandSender arg1) throws TransCommandException
 	{
+		if ( !collection.has(arg0) )
+		{
+			throw new TransCommandException("A user by the name of <f>"+arg0+"<c>does not exist.");
+		}
+		
 		return collection.get(arg0);
 	}
 
