df0bc3caafd877bd7568a7d67e5daa10b730a9b3
lcmcconaghy21 <liam@lcmcconaghy.com>
2020-09-07 23:38:57 +0100

    Can now set profile on join

--- TransCore/src/com/lcmcconaghy/java/transcore/engine/EnginePlayer.java
+++ TransCore/src/com/lcmcconaghy/java/transcore/engine/EnginePlayer.java
@@ -3,15 +3,29 @@ package com.lcmcconaghy.java.transcore.engine;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.lcmcconaghy.java.transcore.Message;
 import com.lcmcconaghy.java.transcore.TransServer;
 import com.lcmcconaghy.java.transcore.gui.ChestGui;
+import com.lcmcconaghy.java.transcore.gui.GuiProfile;
 import com.lcmcconaghy.java.transcore.store.UserCollection;
+import com.lcmcconaghy.java.transcore.store.transcore.IUser;
+import com.lcmcconaghy.java.transcore.store.transcore.TransConfig;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class EnginePlayer extends Engine
 {
+	
+	// { FIELDS } //
+	
+	private Message profileCreate = new Message("Click here to create a new profile.")
+			                            .color( ChatColor.AQUA )
+			                            .suggest("/profile create ");
 	
 	// { SINGLETON } //
 	
@@ -20,18 +34,14 @@ public class EnginePlayer extends Engine
 	
 	// { EVENTS } //
 	
-	@EventHandler(priority=EventPriority.HIGHEST)
+	@EventHandler(priority=EventPriority.NORMAL)
 	public void registerPlayer(PlayerJoinEvent event)
 	{
 		TransServer.get().registerNametag(event.getPlayer());
 		
 		for (UserCollection<?> collection : TransServer.get().getUserCollections())
 		{
-			if (collection.has(event.getPlayer()))
-			{
-				collection.get(event.getPlayer());
-				continue;
-			}
+			if (collection.has(event.getPlayer())) continue;
 			
 			collection.create(event.getPlayer());
 		}
@@ -44,11 +54,53 @@ public class EnginePlayer extends Engine
 	{
 		if (!TransServer.get().isGui( event.getView() )) return;
 		
-		ChestGui gui = TransServer.get().getGui( event.getView() );
+		ChestGui<?> gui = TransServer.get().getGui( event.getView() );
 		ItemStack selection = event.getCurrentItem();
 		
 		Runnable execution = gui.getExecutable(selection);
 		
 		execution.run();
+	}
+	
+	// { PROFILES } //
+	
+	@EventHandler(priority=EventPriority.HIGH)
+	public void profileSelect(PlayerJoinEvent event)
+	{
+		if ( IUser.get( event.getPlayer() ).getActiveProfile() != null ) return;
+		
+		GuiProfile profileUI = GuiProfile.get().build( event.getPlayer() );
+		
+		profileUI.display( event.getPlayer() );
+	}
+	
+	@EventHandler
+	public void actNullProfileMove(PlayerMoveEvent event)
+	{
+		if ( TransConfig.get().allowsActivityWithoutProfile() ) return;
+		if ( IUser.get( event.getPlayer() ).getActiveProfile() != null ) return;
+		
+		IUser user = IUser.get( event.getPlayer() );
+		
+		user.message( new Message("You cannot interact with the world until you create a profile!\n")
+				          .error()
+				          .extra(profileCreate) );
+		
+		event.setCancelled(true);
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void actNullProfileChat(AsyncPlayerChatEvent event)
+	{
+		if ( TransConfig.get().allowsActivityWithoutProfile() ) return;
+		if ( IUser.get( event.getPlayer() ).getActiveProfile() != null ) return;
+		
+		IUser user = IUser.get( event.getPlayer() );
+		
+		user.message( new Message("You cannot chat until you create a profile!\n")
+				          .error()
+				          .extra(profileCreate) );
+		
+		event.setCancelled(true);
 	}
 }
